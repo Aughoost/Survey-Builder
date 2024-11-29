1a9d3a34919e050516bcb7e9a84186ef9386a6e7
August Christopher Paras <44939486+Aughoost@users.noreply.github.com>
2024-11-29 22:00:25 +1300

    Update form.php

--- form.php
+++ form.php
@@ -13,6 +13,7 @@ ob_end_flush();
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="node_modules\jquery\dist\jquery.min.js"></script>
     <script src="node_modules\formBuilder\dist\form-builder.min.js"></script>
     <script src="node_modules\formBuilder\dist\form-render.min.js"></script>
@@ -21,7 +22,7 @@ ob_end_flush();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css" rel="stylesheet">
-        <!-- <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> -->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
         <link rel="stylesheet" href="classes\GoogleForms.css">
     <meta charset="UTF-8">
@@ -40,9 +41,12 @@ ob_end_flush();
           <div class="ctrlqAccent"></div>
           <div class="FormContent1">
      
+
+     
+
           <form id="fb-render" class="fb-render">
           </form>
-         <div class="w-100 d-flex justify-content-center">
+         <div class="w-100 d-flex justify-content-center test">
             <button class="btn btn-primary" style="background-color:#673bb7"form="form-data" id="get-user-data">Sumbit</button>
         </div>
 </div>
@@ -55,12 +59,18 @@ ob_end_flush();
 </html>
 
 <script>
-    
+
+
+
+
+
     const getUserDataBtn = document.getElementById("get-user-data");
     const fbRender = document.getElementById("fb-render");
     // const formbuilderDivClass = document.getElementByClassName("formbuilder-text")[0];
     var currentpage = `<?php include "./forms/" . $code . ".xml" ?>`;
     var Required = true;
+ 
+   
     getUserDataBtn.addEventListener("click", getData);
     function getData() {
         for(var i=0; i < fbRender.elements.length; i++){
@@ -114,4 +124,27 @@ ob_end_flush();
             }
         });
     }
+
+
+    var name = jQuery.trim($(".fb-render .formbuilder-text .SampleClass").val());
+
+    var name = $("#txt").val();
+
+
+
+$(document).ready(function() {
+    $('.fb-render .formbuilder-text .form-control').blur(function(){
+    if( $(this).val().length != 0 ) {
+        $(this).closest('.formbuilder-text').find(".formbuilder-text-label").css({ "color": "#9e9e9e", "font-size": ".75em", "top":"-25px"  });
+        // console.log("test");
+    }
+    if( $(this).val().length == 0 ) {   
+        $(this).closest('.formbuilder-text').find(".formbuilder-text-label").css({ "color": "black", "font-size": "1em", "top":"0"  });
+    }
+    })
+    $('.fb-render .formbuilder-text .form-control').on('click', function() {
+        $(this).closest('.formbuilder-text').find(".formbuilder-text-label").css({ "color": "#9e9e9e", "font-size": ".75em", "top":"-25px"  });
+    });
+});
+
 </script>
